60189268a98123fab9e24be9297c83afcce53d0e
Dmitrii Pochtennov <dmitrii.pochtennov@gmail.com>
2020-04-01 15:48:10 +0200

    isEven game added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import readlineSync from 'readline-sync';
 
+const isEven = (number) => (number % 2 === 0);
+
+const isEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const upperRangeBoundary = 100;
+  for (let i = 0; i < 3; i += 1) {
+    const numberForQuestion = Math.floor(Math.random() * upperRangeBoundary);
+    console.log(`Question: ${numberForQuestion}`);
+    const userInput = readlineSync.question('Your answer: ');
+    const correctAnswer = isEven(numberForQuestion) ? 'yes' : 'no';
+    if (correctAnswer !== userInput) {
+      console.log(`"${userInput}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
+      console.log(`Let's try again, ${name}`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}`);
+};
+
 const greet = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
 };
 
-
-export default greet;
+export { greet, isEvenGame };
