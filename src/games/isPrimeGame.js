f396b403752d5799d58cd771b2a405b65ff452b7
Dmitrii Pochtennov <dmitrii.pochtennov@gmail.com>
2020-04-14 10:27:46 +0200

    Small bug fix in prime game

--- src/games/isPrimeGame.js
+++ src/games/isPrimeGame.js
@@ -5,7 +5,7 @@ const task = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
 const isPrime = (number) => {
   if (number < 2) {
-    return true;
+    return false;
   }
 
   for (let i = 2; i <= number / 2; i += 1) {
